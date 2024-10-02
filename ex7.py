07e757c89d1cec1c2dec556656cd79884cde21b5
juanxto <juanprc1302@gmail.com>
2024-10-02 11:36:36 -0300

    nem tudo foi feito

--- ex7.py
+++ ex7.py
@@ -1,3 +1,11 @@
 cont = 1
 while cont <= 2:
-    n = print('')
+    if cont == 1:
+        n1 = int(input('Digite um valor: '))
+    if cont == 2:
+        n2 = int(input('Digite um valor: '))
+    cont += 1
+if n1 > n2:
+    print(f'O maior valor digitado foi {n1}')
+else:
+    print(f'O maior valor digitado foi {n2}')
---
+++ ex8.py
@@ -0,0 +1,43 @@
+cont = 1
+while cont <= 10:
+    if cont == 1:
+        n1 = float(input('Digite a um numero: '))
+    if cont == 2:
+        n2 = float(input('Digite a um numero: '))
+    if cont == 3:
+        n3 = float(input('Digite a um numero: '))
+    if cont == 4:
+        n4 = float(input('Digite a um numero: '))
+    if cont == 5:
+        n5 = float(input('Digite a um numero: '))
+    if cont == 6:
+        n6 = float(input('Digite a um numero: '))
+    if cont == 7:
+        n7 = float(input('Digite a um numero: '))
+    if cont == 8:
+        n8 = float(input('Digite a um numero: '))
+    if cont == 9:
+        n9 = float(input('Digite a um numero: '))
+    if cont == 10:
+        n10 = float(input('Digite a um numero: '))
+    cont += 1
+if n1 > n2 and n1 > n3 and n1 > n4 and n1 > n5 and n1 > n6 and n1 > n7 and n1 > n8 and n1 > n9 and n1 > n10:
+    print(f'O maior numero digitado foi {n1}')
+if n2 > n1 and n2 > n3 and n2 > n4 and n2 > n5 and n2 > n6 and n2 > n7 and n2 > n8 and n2 > n9 and n2 > n10:
+    print(f'O menor numero digitado foi {n2}')
+if n3 > n1 and n3 > n2 and n3 > n4 and n3 > n5 and n3 > n6 and n3 > n7 and n3 > n8 and n3 > n9 and n3 > n10:
+    print(f'O maior numero digitado foi {n3}')
+if n4 > n1 and n4 > n2 and n4 > n3 and n4 > n5 and n4 > n6 and n4 > n7 and n4 > n8 and n4 > n9 and n4 > n10:
+    print(f'O maior numero digitado foi {n4}')
+if n5 > n1 and n5 > n2 and n5 > n3 and n5 > n4 and n5 > n6 and n5 > n7 and n5 > n8 and n5 > n9 and n5 > n10:
+    print(f'O maior numero digitado foi {n5}')
+if n6 > n1 and n6 > n2 and n6 > n3 and n6 > n4 and n6 >n5 and n6 > n7 and n6 > n8 and n6 > n9 and n6 > n10:
+    print(f'O maior numero digitado foi {n6}')
+if n7 > n1 and n7 > n2 and n7 > n3 and n7 > n4 and n7 > n5 and n7 > n6 and n7 > n8 and n7 > n9 and n7 > n10:
+    print(f'O maior numero digitado foi {n7}')
+if n8 > n1 and n8 > n2 and n8 > n3 and n8 > n4 and n8 > n5 and n8 > n6 and n8 > n7 and n8 > n9 and n8 > n10:
+    print(f'O maior numero digitado foi {n8}')
+if n9 > n1 and n9 > n2 and n9 > n3 and n9 > n4 and n9 > n5 and n9> n6 and n9 > n7 and n9 > n8 and n9 > n10:
+    print(f'O maior numero digitado foi {n9}')
+if n10 > n1 and n10 > n2 and n10 > n3 and n10 > n4 and n10 > n5 and n10 > n6 and n10 > n7 and n10 > n8 and n10 > n9:
+    print(f'O maior numero digitado foi {n10}')
